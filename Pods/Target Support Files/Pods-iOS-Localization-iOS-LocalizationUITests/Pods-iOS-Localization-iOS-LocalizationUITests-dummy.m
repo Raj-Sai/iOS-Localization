0ac340acfe0688e1ae76d8253ba6372d6dbe0f44
Amsaraj Mariyappan <raj.amsarajm93@gmail.com>
2021-09-22 11:47:24 +0700

    initial setup

---
+++ Pods/Target Support Files/Pods-iOS-Localization-iOS-LocalizationUITests/Pods-iOS-Localization-iOS-LocalizationUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS_Localization_iOS_LocalizationUITests : NSObject
+@end
+@implementation PodsDummy_Pods_iOS_Localization_iOS_LocalizationUITests
+@end
---
+++ Pods/Target Support Files/Pods-iOS-LocalizationTests/Pods-iOS-LocalizationTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS_LocalizationTests : NSObject
+@end
+@implementation PodsDummy_Pods_iOS_LocalizationTests
+@end
